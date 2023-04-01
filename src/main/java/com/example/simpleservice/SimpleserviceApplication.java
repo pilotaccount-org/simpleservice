846fe16ba4737e01430c13fc0a285438c68aa3d8
abc <abc@example.com>
2023-04-01 08:59:38 +0530

    First commit

---
+++ src/main/java/com/example/simpleservice/SimpleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleserviceApplication.class, args);
+	}
+
+}
